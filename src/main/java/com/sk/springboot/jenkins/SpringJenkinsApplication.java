d239a42b964690d09f197424ce784a85472e8abb
Susanta <sahoo9404@gmail.com>
2023-12-19 00:06:15 +0530

    first commit

---
+++ src/main/java/com/sk/springboot/jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.sk.springboot.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static Logger log = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@PostConstruct
+	public void intt() {
+		log.info("Appilication started.....");
+	}
+
+	public static void main(String[] args) {
+		log.info("Appilication executed.....");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
